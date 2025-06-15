d014a3b466a4069033c4723d17524639e87c6ba1
Adan <adanshahid.student@gmail.com>
2025-06-15 07:48:18 +0500

    updated javascript folder

---
+++ Javascript/sandbox.js
@@ -0,0 +1,29 @@
+// let age = 20;
+// let year = 2025;
+// console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100; //THIS VARIABLE WILL NOT BE CHANGED LATER.
+
+// console.log(points);
+
+//STRING
+
+console.log("hello, world");
+
+let firstName = "Adan";
+let lastName = "Shahid";
+console.log(firstName +' '+ lastName);
+
+//GETTING CHARACTERS
+console.log(firstName[0]);
+
+//STRING LENGTH
+console.log(firstName.length);
+
+//STRING METHODS
+//METHODS ARE JUST FUNCTIONS THAT BELONG TO THE SPECIFIC OBJECTS.
+
+console.log(firstName.toUpperCase());
