f39731d9787e2856f15ef71494fa97c24b8145cf
Adan <adanshahid.student@gmail.com>
2025-06-17 08:33:43 +0500

    Wrote the first react component

---
+++ Scrimba/first_project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Header(){
+  return <h1>Hello, World!</h1>;
+}
+function App() {
+  return <Header/>;
+}
+  
+
+export default App
